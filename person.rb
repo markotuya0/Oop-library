caf947d6b7b5e651af87a685629472931c213e20
Mark Otuya <markotuya0@gmail.com>
2022-10-11 23:15:26 +0100

    fixed linters

--- person.rb
+++ person.rb
@@ -1,25 +1,19 @@
 class Person
-    attr_accessor :name, :age, :parent_permission
-    attr_reader :id
-  
-    def initialize(name = "Unknown", age, parent_permission: true)
-      @id = Random.rand(1..100)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    private_class_method def self.is_of_age?
-      @age >= 18
-    end
-  
-    def can_use_services?
-      @age >= 18 && @parent_permission
-    end
+  attr_accessor :name, :age, :parent_permission
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  Footer
-  © 2022 GitHub, Inc.
-  Footer navigation
-  Terms
-  Privacy
-  
+
+  private_class_method def self.of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    @age >= 18 && @parent_permission
+  end
+end
