7db7cf41ef06a81ecd886993ea3ee501831ef1ad
Mark Otuya <markotuya0@gmail.com>
2022-10-11 23:29:54 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,19 +1,21 @@
 class Person
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..100)
+  def initialize(age, name: 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  private_class_method def self.of_age?
+  private
+
+  def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    @age >= 18 && @parent_permission
+    of_age? || @parent_permission
   end
 end
